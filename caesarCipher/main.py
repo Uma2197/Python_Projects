2d20bd1fcacc03c0ebc4c38ea06f24fd769330f4
Umamaheswari Nagalingam <124685592+Uma2197@users.noreply.github.com>
2024-07-14 12:45:36 +0200

    Add files via upload

---
+++ caesarCipher/main.py
@@ -0,0 +1,36 @@
+import art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(art.logo)
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            new_letter = alphabet[new_position]
+            end_text += new_letter
+        else:
+            end_text += char
+    print(f"The {cipher_direction}d text is {end_text}")
+# To implement repetition
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift > 26:
+        shift %= 26
+    caesar(text, shift, direction)
+    result = input("Do you want to continue? Type 'Yes' or 'No'")
+    if result == "no":
+        should_continue = False
+        print("Goodbye!")
+
+
