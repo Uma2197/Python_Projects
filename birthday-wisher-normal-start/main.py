45ea70dd81ae5c3453bb1fb8f8fa4f5a6b93a55e
Umamaheswari Nagalingam <124685592+Uma2197@users.noreply.github.com>
2024-07-14 14:28:09 +0200

    Add files via upload

---
+++ birthday-wisher-normal-start/main.py
@@ -0,0 +1,26 @@
+import datetime as dt
+import pandas
+import random
+import smtplib
+
+my_email = "uma.dv1819@gmail.com"
+password = "dfqo rxgg cdcq ozgo"
+now = dt.datetime.now()
+today = (now.month, now.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday!!\n\n{contents}"
+                            )
